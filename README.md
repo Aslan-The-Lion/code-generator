0082db370d1815a1edab94f7bde4ea558114f522
Aslan-The-Lion <alanruffo5050@gmail.com>
2023-10-23 21:59:13 -0700

    added everything, bad practice i know my bad

---
+++ README.md
@@ -0,0 +1,24 @@
+# Alans Password Generator
+
+## Description
+UCSD Coding Challenge - Password generator
+the purpose of this challenge 
+
+## Table of Contents
+1. index.html - contains the wording
+2. style.css - contains the styling of the website
+3. script.js - contains the functions, and information for the generator
+
+## Screenshot & URL
+Deployed Application: 
+GitHub Repo: https://github.com/Aslan-The-Lion/code-generator
+screenshots of website: 
+
+## credit
+Emily Morosoff and and her tutor, Jaba.
+Emily's github: https://github.com/emilymorosoff/password-generator/tree/main
+## License
+Copyright <2023> <Alan Ruffo>
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the “Software”), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
---
+++ assets/script.js
@@ -0,0 +1,64 @@
+var numeric = "0123456789".split(""); 
+var upperCaseLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("");
+var lowerCaseLetters = "abcdefghijklmnopqrstuvwxyz".split("");
+var specialCharacters = "!@#$%^&*()_+~`|}{[]:;?><,./-=".split("");
+
+var letNumeric = false
+var letUppercaseLetters = false
+var letLowerCaseLetters = false
+var letSpecialCharacters = false
+// Get references to the #generate element
+var generateBtn = document.querySelector("#generate");
+function generatePassword() {
+var chooseValues = [];
+var characterAmount = prompt("choose the amount of characters for your password, choose between 8-128")
+ characterAmount = parseInt (characterAmount)
+ 
+ if (isNaN(characterAmount) || characterAmount<8 || characterAmount>128) {
+  return "Invalid Selection, please choose again"
+} else {
+  alert (characterAmount + " characters is the amount you chose.")
+ }
+ letNumeric = confirm ("would you like numbers in your password?")
+ letLowerCaseLetters = confirm ("would you like lowecase letters in your password?")
+ letUppercaseLetters = confirm ("would you like uppercase letters in your password?")
+ letSpecialCharacters = confirm ("would you like special characters in your password?")
+
+ if (!numeric && !letLowerCaseLetters && !letUppercaseLetters && !letSpecialCharacters)
+ return "choose at least one of the values"
+
+ if (letNumeric) {
+  chooseValues = chooseValues.concat (numeric)
+  } if (letLowerCaseLetters) {
+    chooseValues = chooseValues.concat (lowerCaseLetters)
+  } if (letUppercaseLetters) {
+    chooseValues = chooseValues.concat(upperCaseLetters)
+  } if (letSpecialCharacters) {
+    chooseValues = chooseValues.concat (specialCharacters)
+  }
+  
+  var finalpassword = ""
+  
+  
+for(var i = 0; i<characterAmount; i++) {
+  var range = Math.ceil(Math.random()*chooseValues.length) - 1
+    finalpassword += chooseValues[range]
+  }
+  return finalpassword
+}
+
+
+
+
+// Write password to the #password input
+function writePassword() {
+
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword); 
